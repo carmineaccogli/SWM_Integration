2d2c7708c33fe80ce273ba9f73714dcf14d7bfb4
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-07-20 19:15:38 +0200

    Aggiornamento automatico submodules

--- update-submodules.sh
+++ update-submodules.sh
@@ -5,6 +5,7 @@ git submodule update --init --recursive --remote
 if git diff-index --quiet HEAD; then
     echo "Nessun aggiornamento dei submodules."
 else
+    git add .
     git commit -m "Aggiornamento automatico submodules"
     git push origin main
 fi
